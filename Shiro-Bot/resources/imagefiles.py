e643858f8ac66380410fc02265a28ffa5f1db06a
Wahyu <91831925+Wahyu213@users.noreply.github.com>
2021-12-14 21:03:51 +0700

    Create imagefiles.py

---
+++ Shiro-Bot/resources/imagefiles.py
@@ -0,0 +1,6 @@
+# This File Contains all the Picture Links used all over the Bot
+
+#Images in __main__.py
+SHIRO_IMG = "https://telegra.ph/file/48a798d51e32a659edb4e.jpg"
+SHIRO_START_IMG = "https://telegra.ph/file/4fb9af6eb9204c085c7c1.jpg"
+SHIRO_HELP_IMG = "https://telegra.ph/file/d63bf6d0159a649057fce.jpg"
