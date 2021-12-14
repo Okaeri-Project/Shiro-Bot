a136055dc29045850ea0807661c55d88834fc560
Dream Garden (rey) <93763859+HELZRIP@users.noreply.github.com>
2021-12-14 19:41:11 +0700

    Update config.py

--- Shiro-Bot/config.py
+++ Shiro-Bot/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/shiro_bot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Shirorobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
