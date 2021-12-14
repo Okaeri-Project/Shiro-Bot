0e95184c40547c9139dd2eff96bd5bbae0f4d5fe
Wahyu <91831925+Wahyu213@users.noreply.github.com>
2021-12-14 20:52:31 +0700

    Update keyboard.py

--- Shiro-Bot/utils/keyboard.py
+++ Shiro-Bot/utils/keyboard.py
@@ -1,7 +1,7 @@
 from pykeyboard import InlineKeyboard
 from pyrogram.types import InlineKeyboardButton as Ikb
 
-from ShiroNeeBot.utils.functions import get_urls_from_text as is_url
+from Shirobot.utils.functions import get_urls_from_text as is_url
 
 
 def keyboard(buttons_list, row_width: int = 2):
